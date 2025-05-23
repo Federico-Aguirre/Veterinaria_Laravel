78794ae8b69913f4493a9ba5eaec49f926b84d26
Federico-Aguirre <sedent333@gmail.com>
2025-05-23 05:20:47 -0300

    a

--- config/database.php
+++ config/database.php
@@ -95,9 +95,7 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
-            'options' => extension_loaded('pdo_pgsql') ? [
-                PDO::PGSQL_ATTR_SSL_MODE => env('DB_SSLMODE', 'prefer'),
-            ] : [],
+            'options' => [],
         ],
 
         'sqlsrv' => [
