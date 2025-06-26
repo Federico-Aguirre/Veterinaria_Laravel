b82218096b7c45c2289d04d082c4aa4a702125dd
Federico-Aguirre <sedent333@gmail.com>
2025-06-26 08:39:43 -0300

    a

--- resources/js/ver_datos_de_perfil.js
+++ resources/js/ver_datos_de_perfil.js
@@ -1,22 +1,24 @@
-document.getElementById('ver-perfil-btn').addEventListener('click', function () {
-    fetch('/ver-perfil')
-        .then(response => response.json())
-        .then(data => {
-            let mensaje = `
-                Nombre: ${data.name}
-                Apellido: ${data.apellido}
-                Dirección: ${data.direccion}
-                Piso: ${data.piso}
-                Departamento: ${data.departamento}
-                Localidad: ${data.localidad}
-                Teléfono: ${data.telefono}
-                Celular: ${data.celular}
-                Email: ${data.email}
-            `;
-            alert(mensaje);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('Error al obtener el perfil');
-        });
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('ver-perfil-btn').addEventListener('click', function () {
+        fetch('/ver-perfil')
+            .then(response => response.json())
+            .then(data => {
+                let mensaje = `
+                    Nombre: ${data.name}
+                    Apellido: ${data.apellido}
+                    Dirección: ${data.direccion}
+                    Piso: ${data.piso}
+                    Departamento: ${data.departamento}
+                    Localidad: ${data.localidad}
+                    Teléfono: ${data.telefono}
+                    Celular: ${data.celular}
+                    Email: ${data.email}
+                `;
+                alert(mensaje);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert('Error al obtener el perfil');
+            });
+    });
 });
