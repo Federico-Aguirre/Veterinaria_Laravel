38d134673481ad7ca6b1bf4174d2417cc19d49e3
Federico-Aguirre <sedent333@gmail.com>
2025-06-28 06:08:16 -0300

    a

--- app/Models/MascotaModel.php
+++ app/Models/MascotaModel.php
@@ -14,8 +14,8 @@ class MascotaModel extends Model
     protected $primaryKey = 'id';
 
     protected $fillable = [
-        'nombre', 'raza', 'sexo', 'edad', 'nro_de_microchip', 'vacuna_antirrábica', 
-        'tratamiento_antiparasitario', 'otras_vacunas', 'información_adicional', 'id_user'
+        'nombre', 'raza', 'sexo', 'edad', 'nro_de_microchip', 'vacuna_antirrabica', 
+        'tratamiento_antiparasitario', 'otras_vacunas', 'informacion_adicional', 'id_user'
     ];
 
     // Relación con el modelo User
