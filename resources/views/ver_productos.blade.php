1432f165447e90ba2e3f5e0ad061b5c8ba34fc58
Federico-Aguirre <sedent333@gmail.com>
2025-08-10 23:55:04 -0300

    a

--- resources/views/ver_productos.blade.php
+++ resources/views/ver_productos.blade.php
@@ -74,7 +74,7 @@
 
     <button id="btn-agregar-global" class="fixed left-1/2 bg-green hover:bg-skyBlue
         text-white font-bold py-2 px-4 rounded transition duration-300 w-[200px] z-50"
-        style="transform: translateX(-50%) translateY(80vh);">
+        style="top: 80vh; transform: translateX(-50%);">
         Agregar al carro
     </button>
 </section>
