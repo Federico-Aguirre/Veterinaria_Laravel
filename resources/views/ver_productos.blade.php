1c16b764534bdefa92319d9305462e0c9ed41999
Federico-Aguirre <sedent333@gmail.com>
2025-08-10 22:11:45 -0300

    a

--- resources/views/ver_productos.blade.php
+++ resources/views/ver_productos.blade.php
@@ -72,7 +72,7 @@
         @endif
     </div>
 
-    <button id="btn-agregar-global" class="fixed top-[90vh] left-1/2 transform -translate-x-1/2 bg-green hover:bg-skyBlue
+    <button id="btn-agregar-global" class="fixed top-[calc(90vh)] left-1/2 transform -translate-x-1/2 bg-green hover:bg-skyBlue
         text-white font-bold py-2 px-4 rounded transition duration-300 w-[200px] z-50">
         Agregar al carro
     </button>
